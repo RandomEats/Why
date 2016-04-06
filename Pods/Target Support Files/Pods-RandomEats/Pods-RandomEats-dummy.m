5e221cf1a51def8da7e6609894d414b1bc2f0de5
MissDroid <memissdroid@gmail.com>
2016-04-06 17:53:22 -0400

    WHYYYY

---
+++ Pods/Target Support Files/Pods-RandomEats/Pods-RandomEats-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RandomEats : NSObject
+@end
+@implementation PodsDummy_Pods_RandomEats
+@end
---
+++ RandomEats/YelpAPI.swift
@@ -0,0 +1,88 @@
+//
+//  YelpAPI.swift
+//  Yelp
+//
+//  Created by Timothy Lee on 9/19/14.
+//  Copyright (c) 2014 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+import AFNetworking
+import BDBOAuth1Manager
+
+// You can register for Yelp API keys here: http://www.yelp.com/developers/manage_api_keys
+let yelpConsumerKey = "4AsuYcoVTGLJantOl4mkhg"
+let yelpConsumerSecret = "RWMc2VZJYWwvyfS7vijbuUhu3Mk"
+let yelpToken = "WnStMN3BH2OAExXYTwk7DOzpx15pZRNE"
+let yelpTokenSecret = "qntqJQSJO6s0jGeVP5K0sT1iQCQ"
+
+enum YelpSortMode: Int {
+    case BestMatched = 0, Distance, HighestRated
+}
+
+class YelpAPI: BDBOAuth1RequestOperationManager
+{
+    var accessToken: String!
+    var accessSecret: String!
+    
+    class var sharedInstance : YelpAPI {
+        struct Static {
+            static var token : dispatch_once_t = 0
+            static var instance : YelpAPI? = nil
+        }
+        
+        dispatch_once(&Static.token) {
+            Static.instance = YelpAPI(consumerKey: yelpConsumerKey, consumerSecret: yelpConsumerSecret, accessToken: yelpToken, accessSecret: yelpTokenSecret)
+        }
+        return Static.instance!
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    init(consumerKey key: String!, consumerSecret secret: String!, accessToken: String!, accessSecret: String!) {
+        self.accessToken = accessToken
+        self.accessSecret = accessSecret
+        let baseUrl = NSURL(string: "https://api.yelp.com/v2/")
+        super.init(baseURL: baseUrl, consumerKey: key, consumerSecret: secret);
+        
+        let token = BDBOAuth1Credential(token: accessToken, secret: accessSecret, expiration: nil)
+        self.requestSerializer.saveAccessToken(token)
+    }
+    
+    func searchWithTerm(term: String, completion: ([Business]!, NSError!) -> Void) -> AFHTTPRequestOperation {
+        return searchWithTerm(term, sort: nil, categories: nil, deals: nil, completion: completion)
+    }
+    
+    func searchWithTerm(term: String, sort: YelpSortMode?, categories: [String]?, deals: Bool?, completion: ([Business]!, NSError!) -> Void) -> AFHTTPRequestOperation {
+        // For additional parameters, see http://www.yelp.com/developers/documentation/v2/search_api
+        
+        // Default the location to San Francisco
+        var parameters: [String : AnyObject] = ["term": term, "ll": "37.785771,-122.406165"]
+        
+        if sort != nil {
+            parameters["sort"] = sort!.rawValue
+        }
+        
+        if categories != nil && categories!.count > 0 {
+            parameters["category_filter"] = (categories!).joinWithSeparator(",")
+        }
+        
+        if deals != nil {
+            parameters["deals_filter"] = deals!
+        }
+        
+        print(parameters)
+        
+        return self.GET("search", parameters: parameters, success: { (operation: AFHTTPRequestOperation!, response: AnyObject!) -> Void in
+            let dictionaries = response["businesses"] as? [NSDictionary]
+            if dictionaries != nil {
+                completion(Business.businesses(array: dictionaries!), nil)
+            }
+            }, failure: { (operation: AFHTTPRequestOperation?, error: NSError!) -> Void in
+                completion(nil, error)
+        })!
+    }
+}
